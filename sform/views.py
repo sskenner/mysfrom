693e92d7c086a9bfa8e0118e44e47b68e2103fbd
skA <sskenner@gmail.com>
2018-05-01 22:37:56 -0400

    update to render bootstrap 4 forms

--- sform/views.py
+++ sform/views.py
@@ -3,9 +3,11 @@ from .forms import ColorfulContactForm, ContactForm
 
 def home(request):
     if request.method == 'POST':
-        form = ColorfulContactForm(request.POST)
+        # form = ColorfulContactForm(request.POST)
+        form = ContactForm(request.POST)
         if form.is_valid():
             pass  # does nothing, just trigger the validation
     else:
-        form = ColorfulContactForm()
+        # form = ColorfulContactForm()
+        form = ContactForm()
     return render(request, 'home.html', {'form': form})
